52582c0b84b73dcc08368572557f3834461456f8
the-madhankumar <madhankumarbusiness@gmail.com>
2026-07-23 19:53:08 +0530

    complete: completed Postpaid bill system

--- 7_Postpaid_Billing/routes/payment.py
+++ 7_Postpaid_Billing/routes/payment.py
@@ -4,8 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from database import get_db
-from models import Customer, Bill
-from schemas import GenerateBillRequest, BillResponse
+from models import Customer, Bill, Payment
+from schemas import GenerateBillRequest, BillResponse, PaymentResponse, PayBillRequest
 from schemas import CustomerCreate, CustomerResponse
 
 router = APIRouter()
@@ -37,14 +37,6 @@ def generate_bill(data: GenerateBillRequest, db: Session = Depends(get_db)):
 
     today = date.today()
 
-    existing_bill = (
-        db.query(Bill)
-        .filter(
-            Bill.customer_id == data.customer_id,
-        )
-        .first()
-    )
-
     bill = Bill(
         customer_id=data.customer_id,
         amount=data.amount,
@@ -60,3 +52,49 @@ def generate_bill(data: GenerateBillRequest, db: Session = Depends(get_db)):
     db.refresh(bill)
 
     return bill
+
+@router.post("/pay-bill", response_model=PaymentResponse)
+def pay_bill(data: PayBillRequest, db: Session = Depends(get_db)):
+    bill = db.get(Bill, data.bill_id)
+    if bill is None:
+        raise HTTPException(status_code=404, detail="Bill Not Found")
+    
+    new_payment = Payment(
+        bill_id=data.bill_id,
+        amount=data.amount,
+        transaction_id=data.transaction_id
+    )
+
+    db.add(new_payment)
+
+    db.commit()
+    db.refresh(new_payment)
+
+    return {
+        "Status": "Payment Successfull",
+        "data": new_payment
+    }
+
+@router.get("/bill/{bill_id}")
+def get_bill(bill_id: int, db: Session = Depends(get_db)):
+    bill = db.get(Bill, bill_id)
+
+    if bill is None:
+        return HTTPException(status_code=404, detail="No bill Found")
+
+    return bill
+
+@router.get("/bill/history/{customer_id}")
+def get_customer_bills(customer_id: int, db: Session = Depends(get_db)):
+    customer = db.get(Customer, customer_id)
+
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+    bills = (
+        db.query(Bill)
+        .filter(Bill.customer_id == customer_id)
+        .all()
+    )
+
+    return bills
